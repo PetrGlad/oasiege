e38588cb0d52dca1c275b295011fc12364ed1e7d
Petr Gladkikh <petr.gladkikh@zalando.de>
2016-12-21 16:43:53 +0100

    Better idea for spec forms, WIP

--- src/oasiege/core.clj
+++ src/oasiege/core.clj
@@ -14,7 +14,6 @@
 
 (def ere (s/conform :swagger/definition
            (yaml/parse-string (slurp "test/oasiege/data/hummus.yaml"))))
-;;;; (gen/sample (s/gen ::hummus))
 
 (def generated-request-record ;;; example
   {:path "/label/{key}"
@@ -42,31 +41,84 @@
     (dissoc generated-request ::parameters)
     (::parameters generated-request)))
 
-(defn declare-spec [prefix key spec]
-  (let [spec-key (keyword (namespace prefix)
-                   (str (name prefix) "." (name key)))]
-    (eval `(s/def ~spec-key ~spec))))
+(defn prefixed-key [prefix key]
+  (keyword (namespace prefix)
+    (str (name prefix) "." (name key))))
 
-(defn declare-hummus-top! [prefix top-keys]
-  (declare-spec prefix :api `(s/keys :req [~@top-keys]))
-  (s/def ::path #{"/label/{key}" "/number/{key}" "/other-number/{key}" "/event-types/{key}/events"})
-  (s/def ::method #{:get :post :delete})
-  (s/def ::parameters (s/keys :req [::key]))
-  (s/def ::key (s/keys :req [::value ::in]))
-  (s/def ::value string?)
-  (s/def ::in #{:path}))
+(defn values-to-keys [key-prefix values]
+  (->> values
+    (map-indexed
+      (fn [i v]
+        [(prefixed-key key-prefix (str i)) v]))
+    (into {})))
+
+(defn define-spec [spec-key spec]
+  ;; We could use s/def-impl do add definitions but then spec's explanation functions
+  ;; that show predicates' source would not work correctly.
+  (eval `(s/def ~spec-key ~spec)))
+
+(defn declare-specs! [definition prefix]
+  (let [skey #(prefixed-key prefix %)
+        sdef (fn [sub-key spec-form]
+               (define-spec (skey sub-key) spec-form))]
+    ;; The struct we want to generate:
+    (sdef :api-call `(s/keys :req [(skey :path) (skey :method) (skey :parameters)]))
+
+    (let [path-keys (->> definition :paths keys (values-to-keys prefix))]
+      (sdef :path (into #{} (vals path-keys))))
+    (sdef :method #{:get :post :delete})
+    (sdef :parameters (s/keys :req [(skey :key)]))
+    (sdef :key (s/keys :req [(skey :value) (skey :in)]))
+    (sdef :value string?)
+    (sdef :in #{:path})
+
+    ;; !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    ;; Better spec form: match sequences instead of assocs,
+    ;; use s/tuple to tag choices.
+    ;; !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    (gen/sample
+      (s/gen
+        (s/alt
+          :path1 (s/tuple #{"path1"}
+                   (s/alt
+                     :get (s/tuple #{:get}
+                            (s/cat
+                              :a (s/tuple #{"pa"} string?)
+                              :b (s/? (s/tuple #{"pb"} integer?))))
+                     :post (s/tuple #{:post}
+                             (s/cat
+                               :a (s/tuple #{"name"} string?)
+                               :b (s/? (s/tuple #{"id"} integer?))))))
+          :path2 (s/tuple #{"path2"}
+                   (s/alt
+                     :get (s/tuple #{:get}
+                            (s/cat
+                              :a (s/tuple #{"p2a"} string?)
+                              :b (s/? (s/tuple #{"p2b"} integer?))))
+                     :post (s/tuple #{:post}
+                             (s/cat
+                               :a (s/tuple #{"name"} string?)
+                               :b (s/? (s/tuple #{"id"} integer?)))))))))
+
+
+    (skey :api-call)))
+
+(defonce serial-id (atom 0))
+
+(def new-spec-prefix! []
+  (keyword (str *ns*) (str "z" (swap! serial-id inc))))
 
 (defn proof-of-concept-run []
   (let [definition (s/conform :swagger/definition
-                     (yaml/parse-string (slurp "test/oasiege/data/hummus.yaml")))]
-    ;; Can parse OpenAPI definition
-    (prn definition))
-  (declare-hummus-top! ::hummus #{::path ::method ::parameters})
-  ;; Generate request records from hand-crafted specs corresponding to the definition
-  (let [generated-records (gen/sample (s/gen ::hummus.api) 10)]
-    (prn generated-records)
-    ;; Embed parameters to make generated records suitable for HTTP calls
-    (prn (map prepare-request generated-records))))
+                     (yaml/parse-string (slurp "test/oasiege/data/hummus.yaml")))
+        prefix (new-spec-prefix!)]
+    (assert (clojure.string/starts-with? (:swagger definition) "2.")) ;; just in case
+    (declare-specs! definition prefix)
+    ;; Generate request records from hand-crafted specs corresponding to the definition
+    (let [generated-records (gen/sample (s/gen ::hummus.api) 10)]
+      (prn generated-records)
+      ;; Embed parameters to make generated records suitable for HTTP calls
+      (prn (map prepare-request generated-records)))))
 
 (comment
   (-> ere
